88cea2f89e3578f6e0ea2b105149f354ce39951a
Louis-dobrowolski-hwu <113677079+Louis-dobrowolski-hwu@users.noreply.github.com>
2022-11-24 12:09:07 +0000

    add project .C

---
+++ projet/main_dobrowolski.c
@@ -0,0 +1,141 @@
+#include "avr/io.h"
+
+void initADC(void);
+void USART_init(void);
+unsigned char USART_receive(void);
+
+#define BAUDRATE 9600
+#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
+
+
+uint16_t potentiometerValue;
+uint16_t threshold_level;
+
+bool flag;
+
+int incr=0; 
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+
+//Main function
+void main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+
+  //H-bridge direction control
+  //DDRD |= (1 << 2) | (1 << 3)| (1 << 4) | (1 << 7);
+  DDRD= 0x11011100;
+  DDRB = 0b00000001;
+  DDRC = 0b0000000;
+  //OCR0A PWM, set PortD.6 output
+  DDRD |= (1 << 6);
+  //PORTD = 0x08;
+  //set OCR0A PWM, fast PWM, no
+  //prescaler, non-inverted
+  TCCR0B &= 0b11111000;
+  TCCR0B |= 0b00000101;//prescaler de 1024//probléme
+  TCCR0A = 0b10100011;// normal non inversé et Fast PWM 
+
+  DDRB |= (1 << PB2); //Data Direction Register B: writing a 1 to the bit enables output
+  threshold_level= 0b10000000;
+  initADC();  
+  USART_init();
+
+  //loop
+  while(1){
+    unsigned char ReceivedChar = USART_receive();
+
+    //ultrasonic sensor control 
+    PORTB &= ~  (1<<0);
+    _delay_ms(0.002);
+    PORTB |= (1<<0);
+    _delay_ms(0.01);
+    PORTB &= ~  (1<<0);
+    duration = pulseIn(9, HIGH);
+    distance = duration * 0.034 / 2;
+
+    //USART commande 
+    if (ReceivedChar == 's' & flag==true)
+      {
+
+      PORTD = PORTD &= ~ (1<<2);
+      PORTD = PORTD  |= (1<<3);
+      PORTD = PORTD  |= (1<<4);
+      PORTD = PORTD &= ~(1<<7);
+    }
+    else if(ReceivedChar == 'z' & flag==true){
+     
+      PORTD = PORTD |= (1<<2);
+      PORTD = PORTD &= ~  (1<<3);
+      PORTD = PORTD &= ~  (1<<4);
+      PORTD = PORTD |= (1<<7);
+    }
+    else if(ReceivedChar == 'q' & flag==true){
+      PORTD = PORTD |= (1<<2);
+      PORTD = PORTD &= ~  (1<<3);
+      PORTD = PORTD  |= (1<<4);
+      PORTD = PORTD &= ~(1<<7);
+    }
+    else if(ReceivedChar == 'd' & flag==true){
+      PORTD = PORTD &= ~ (1<<2);
+      PORTD = PORTD  |= (1<<3);
+      PORTD = PORTD &= ~  (1<<4);
+      PORTD = PORTD |= (1<<7);
+    }
+    else if(ReceivedChar == 'a' & flag==true){
+      PORTD = PORTD |= (1<<2);
+      PORTD = PORTD |= (1<<3);
+      PORTD = PORTD |=   (1<<4);
+      PORTD = PORTD |= (1<<7);
+    }
+    if(distance<10){
+      PORTD = PORTD |= (1<<2);
+      PORTD = PORTD |= (1<<3);
+      PORTD = PORTD |=   (1<<4);
+      PORTD = PORTD |= (1<<7);
+    }
+
+    //ADC convertion
+    ADCSRA |= (1 << ADSC); //start ADC conversion
+
+    while((ADCSRA & (1 << ADSC))) //wait until ADSC bit is clear, i.e., ADC conversion is done
+    {}
+    //read ADC value in
+    uint8_t theLowADC = ADCL;
+    potentiometerValue = ADCH << 8 | theLowADC;
+    potentiometerValue = ADC;
+    
+    OCR0A=potentiometerValue /4;
+    
+
+  }
+}(void)
+
+//initialisation ADC function
+void initADC(void) {
+  ADMUX |= (1 << REFS0) | (1 << MUX0); //reference voltage on AVCC, and use ADC1
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS0); //ADC clock prescaler / 8
+  ADCSRA |= (1 << ADEN); //enables the ADC
+}
+
+//Initialisation USART
+void USART_init(void){
+  UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8); //set baud rate
+  UBRR0L = (uint8_t)(BAUD_PRESCALLER);
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0); //enable transmit
+  UCSR0C = (3<<UCSZ00); //set 8-bit (default)
+}
+
+//USART reciep function
+unsigned char USART_receive(void){
+  if(!(UCSR0A & (1<<RXC0))){
+     flag=false;
+  } // Wait to receive data
+  else{
+
+    flag=true;
+  }
+  return UDR0; // Read data from UDR
+}
